59456bbc0f79335c86928783580543cabd72a221
Martin Kompf <skaringa@nospam.org>
2013-01-09 14:11:19 +0100

    more wellknown rivers added

--- src/com/skaringa/riversystem/WellknownRivers.java
+++ src/com/skaringa/riversystem/WellknownRivers.java
@@ -14,6 +14,15 @@ public class WellknownRivers {
 		id2Basin = new HashMap<Long, String>();
 		id2Basin.put(102135980L, "Rhein");
 		id2Basin.put(128043705L, "Weser");
+		id2Basin.put(4308645L, "Ems");
+		id2Basin.put(5011091L, "Donau");
+		id2Basin.put(30613639L, "Elbe");
+		id2Basin.put(89253786L, "Oder");
+		id2Basin.put(156293030L, "Warnow");
+		id2Basin.put(5005413L, "Trave");
+		id2Basin.put(4267187L, "Schlei");
+		id2Basin.put(5011995L, "Eider");
+		id2Basin.put(36054759L, "Maas"); // Rur
 	}
 
 	public static String getBasin(long riverId) {
